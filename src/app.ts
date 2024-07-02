d3dcea55bed530f670c2e3564b98736f6a4482e2
BeaconBacon <62158062+BeaconBacon@users.noreply.github.com>
2024-07-02 13:11:57 -0400

    decorator stuff

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 function Logger(logString: string) {
-  console.log('LOGGER FACTORY');
+  console.log("LOGGER FACTORY");
   return function (constructor: Function) {
     console.log(logString);
     console.log(constructor);
@@ -7,19 +7,19 @@ function Logger(logString: string) {
 }
 
 function WithTemplate(template: string, hookId: string) {
-  return function(constructor: any) {
-    console.log('Rendering template');
+  return function (constructor: any) {
+    console.log("Rendering template");
     const hookEl = document.getElementById(hookId);
     const p = new constructor();
     if (hookEl) {
       hookEl.innerHTML = template;
-      hookEl.querySelector('h1')!.textContent = p.name;
+      hookEl.querySelector("h1")!.textContent = p.name;
     }
-  }
+  };
 }
 
-@Logger('LOGGING')
-@WithTemplate('<h1>My Person Object</h1>', 'app')
+@Logger("LOGGING")
+@WithTemplate("<h1>My Person Object</h1>", "app")
 class Person {
   name = "Max";
 
@@ -31,3 +31,37 @@ class Person {
 const pers = new Person();
 
 console.log(pers);
+
+function Log(target: any, propertyName: string | Symbol) {
+  console.log("Property decorator!");
+  console.log(target, propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log('Accessor decorator');
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+class Product {
+  @Log
+  title: string;
+  private _price: number;
+
+  @Log2
+  set price(val: number) {
+    if (val > 0) {
+      this._price = val;
+    } else {
+      throw new Error("Invalid price - should be positive!");
+    }
+  }
+  constructor(t: string, p: number) {
+    this.title = t;
+    this._price = p;
+  }
+  getPricewithTax(tax: number) {
+    return this._price * (1 + tax);
+  }
+}
